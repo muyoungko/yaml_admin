994e43aed8eed5c19ed1ccc2bef82ada34ce73d1
muyoungko <muyoungko@example.com>
2026-04-01 13:35:03 +0900

    react dom duplcate

--- example/front1/vite.config.js
+++ example/front1/vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: [
+      'react',
+      'react-dom',
+      'react-router',
+      'react-router-dom',
+      'react-admin',
+      'ra-core',
+    ],
+  },
   server: {
     port: 6900,
     proxy: {
