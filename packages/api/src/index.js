b172da7609b33a5bce53b84fe2732f113c3ddede
muyoungko <muyoungko@example.com>
2026-05-14 11:41:06 +0900

    version up

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,5 +1,6 @@
 const registerRoutes = require('./yml-admin-api.js');
 const { genEntityIdWithKey } = require('./common/util.js');
+const { withConfig } = require('./login/auth.js');
 const { withConfigLocal } = require('./upload/localUpload.js');
 const { withConfigS3 } = require('./upload/s3Upload.js');
-module.exports = { registerRoutes, genEntityIdWithKey, withConfigLocal, withConfigS3 };
+module.exports = { registerRoutes, genEntityIdWithKey, withConfigLocal, withConfigS3, withConfig };
