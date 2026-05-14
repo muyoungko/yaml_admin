3ee1ef0a01a8bd949927dd8470f798162e31e066
muyoungko <muyoungko@example.com>
2026-05-14 13:02:32 +0900

    Uploader singleton

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,6 +1,4 @@
 const registerRoutes = require('./yml-admin-api.js');
 const { genEntityIdWithKey } = require('./common/util.js');
-const { withConfig } = require('./login/auth.js');
-const { withConfigLocal } = require('./upload/localUpload.js');
-const { withConfigS3 } = require('./upload/s3Upload.js');
-module.exports = { registerRoutes, genEntityIdWithKey, withConfigLocal, withConfigS3, withConfig };
+const { Uploader } = require('./common/Uploader.js');
+module.exports = { registerRoutes, genEntityIdWithKey, Uploader };
